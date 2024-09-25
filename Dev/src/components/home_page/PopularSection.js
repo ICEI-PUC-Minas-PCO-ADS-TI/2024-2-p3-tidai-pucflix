3be80f67f377af56343fefe928582d7e92dae6d7
Caio Ferreira Araujo <caiof13araujo@gmail.com>
2024-09-25 02:27:04 -0300

    Ajustes e inicio section final da home page

---
+++ Dev/src/components/home_page/PopularSection.js
@@ -0,0 +1,13 @@
+import '../../assets/css/home_page/PopularSection.css';
+
+function PopularSection() {
+    return (
+
+        <div className='PopularSection'>
+            <h1>Titulos de Sucesso no nosso catalogo</h1>
+        </div>
+
+    );
+}
+
+export default PopularSection;
--- Dev/src/pages/home_page/Home_page.js
+++ Dev/src/pages/home_page/Home_page.js
@@ -1,11 +1,13 @@
 import BannerSection from '../../components/home_page/BannerSection';
 import InfoSection from '../../components/home_page/InfoSection';
+import PopularSection from '../../components/home_page/PopularSection';
 
 function home_page(){
     return(
         <div>
             <BannerSection />
             <InfoSection />
+            <PopularSection />
         </div>
     )
 }
