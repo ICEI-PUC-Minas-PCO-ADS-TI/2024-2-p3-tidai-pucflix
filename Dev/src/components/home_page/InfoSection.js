e0fbc5c51595dd52bcc70465eedb049e74924f1f
Caio Ferreira Araujo <caiof13araujo@gmail.com>
2024-09-25 01:52:53 -0300

    Nova Section

---
+++ Dev/src/components/home_page/InfoSection.js
@@ -0,0 +1,20 @@
+import '../../assets/css/home_page/InfoSection.css';
+import Imagem from '../../assets/imagens/InfoSection.png';
+
+function InfoSection() {
+    return (
+
+        <div className='InfoSection'>
+            <h1>Descubra Novas Aventuras com os filmes</h1>
+            <div className='container'>
+                <img src={Imagem} alt="" />
+                <p>
+                    Mergulhe em um universo cinematográfico repleto de aventuras, emoções e histórias inesquecíveis. Nossa plataforma oferece uma curadoria única de filmes e séries que vão desde clássicos atemporais até as últimas estreias. Navegue facilmente por uma vasta seleção de gêneros, encontre suas obras favoritas e se conecte com uma comunidade apaixonada por entretenimento. Prepare-se para explorar novas narrativas que cativarão sua imaginação e proporcionarão momentos de puro deleite.
+                </p>
+            </div>
+        </div>
+
+    );
+}
+
+export default InfoSection;
--- Dev/src/pages/home_page/Home_page.js
+++ Dev/src/pages/home_page/Home_page.js
@@ -1,10 +1,11 @@
 import BannerSection from '../../components/home_page/BannerSection';
-
+import InfoSection from '../../components/home_page/InfoSection';
 
 function home_page(){
     return(
         <div>
             <BannerSection />
+            <InfoSection />
         </div>
     )
 }
