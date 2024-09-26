8b57ed38baa98e3531e0c9c51475d6ad672968ea
Manuela de Souza <manueladesouzaassis@outlook.com>
2024-09-26 09:40:35 -0300

    sobreposição de imagem nos cards

--- Dev/src/pages/Pag_gerenciamento_perfis.js
+++ Dev/src/pages/Pag_gerenciamento_perfis.js
@@ -2,27 +2,35 @@ import Cards from "../components/Cards";
 import styles  from "../assets/css/Pag_gerenciamento_perfis.module.css";
 
 
+
 function Pag_gerenciamento_perfis(){
+    const cards = [{
+        nome:'Usuario1',
+        foto:'https://via.placeholder.com/200'
+
+    },
+    {
+        nome:'Usuario2',
+        foto:'https://via.placeholder.com/200'
+    },
+    {
+        nome:'Usuario3',
+        foto:'https://via.placeholder.com/200'
+    },
+    {
+        nome:'Usuario4',
+        foto:'https://via.placeholder.com/200'
+    }]
     return(
-        
-        
             < div className={styles.conteudo}>
                 
                 <h1 className={styles.titulo}>Gerenciar perfis</h1>
                 <div className={styles.perfis}>
-                    <Cards  nome="Nome usuário" 
-                    foto="https://via.placeholder.com/200"
-                    />
-                    <Cards  nome="Nome usuário" 
-                     foto="https://via.placeholder.com/200"
-                    />
-                    <Cards  nome="Nome usuário" 
-                    foto="https://via.placeholder.com/200"
-                    />
-                    <Cards  nome="Nome usuário" 
-                    foto="https://via.placeholder.com/200"
-                    />
-                     
+                    {cards.map(card=>(
+                        <Cards  nome={card.nome} 
+                        foto={card.foto}
+                        />
+                    ))}  
                 </div>
 
             </div>
