206f2a2a7ff75323acbcbf1a38bbb96f5551bfeb
ItalloOtavio <otavio.itallo@gmail.com>
2024-09-27 22:34:16 -0300

    HTML PopUp filmes

---
+++ Dev/src/pages/PopUpFIlmes.js
@@ -0,0 +1,14 @@
+function PopUp(){
+    const url = 'https://via.placeholder.com/150'
+    return(
+        <div>
+            <img class="" src={url} alt="..."></img>
+            <h1>Nome do Filme</h1>
+            <p>Descrição..............................</p>
+            <h6>ANO | tempo</h6>
+            <button>Assistir</button>
+            <button>Adicionar aos favoritos</button>
+        </div>
+    )
+}
+export default PopUp;
