cb1a17f6a4ef7911f5d6bfb9163afc574a7fa27d
Caio Ferreira Araujo <caiof13araujo@gmail.com>
2024-09-27 23:28:45 -0300

    carrossel da home-page

---
+++ Dev/src/components/home_page/Carrossel.js
@@ -0,0 +1,38 @@
+import '../../assets/css/home_page/Carrossel.css';
+import { motion } from 'framer-motion';
+import { useState, useEffect, useRef } from 'react'
+
+//Realiza a importaçao de todas imagens que estiverem na pasta de imagens do carrossel
+const importAll = r => r.keys().map(r);
+const filmes = importAll(require.context('../../assets/imagens/Carrossel', false, /\.(png|jpe?g|svg)$/));
+
+function Carrossel() {
+
+    const carrossel = useRef();
+    const [width, setWidth] = useState(0)
+
+    useEffect(() => {
+        if (carrossel.current) {
+            setWidth(carrossel.current.scrollWidth - carrossel.current.offsetWidth);
+        }
+    }, []);
+    return (
+
+        <div className='conteudo-carrossel'>
+            < motion.div ref={carrossel} className='carrossel'>
+                < motion.div className='interno' drag="x" dragConstraints={{ right: 0, left: -width }}>
+
+                    {filmes.map(filme => (
+                        <motion.div className='item' key={filme}>
+                            <img src={filme} alt='' />
+                        </motion.div>
+                    ))}
+
+                </motion.div>
+            </motion.div>
+        </div>
+
+    );
+}
+
+export default Carrossel;
--- Dev/src/components/home_page/PopularSection.js
+++ Dev/src/components/home_page/PopularSection.js
@@ -1,10 +1,12 @@
 import '../../assets/css/home_page/PopularSection.css';
+import Carrossel from './Carrossel';
 
 function PopularSection() {
     return (
 
         <div className='PopularSection'>
             <h1>Titulos de Sucesso no nosso catalogo</h1>
+            <Carrossel />
         </div>
 
     );
