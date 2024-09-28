a47ddd09ef235235a0992fc2dc455cb2502d229e
Caio Ferreira Araujo <caiof13araujo@gmail.com>
2024-09-28 00:48:30 -0300

    Perguntas

--- Dev/src/components/home_page/InfoSection.js
+++ Dev/src/components/home_page/InfoSection.js
@@ -6,7 +6,7 @@ function InfoSection() {
 
         <div className='InfoSection'>
             <h1>Descubra Novas Aventuras com os filmes</h1>
-            <div className='container'>
+            <div className='container-info'>
                 <img src={Imagem} alt="" />
                 <p>
                     Mergulhe em um universo cinematográfico repleto de aventuras, emoções e histórias inesquecíveis. Nossa plataforma oferece uma curadoria única de filmes e séries que vão desde clássicos atemporais até as últimas estreias. Navegue facilmente por uma vasta seleção de gêneros, encontre suas obras favoritas e se conecte com uma comunidade apaixonada por entretenimento. Prepare-se para explorar novas narrativas que cativarão sua imaginação e proporcionarão momentos de puro deleite.
--- Dev/src/components/home_page/PopularSection.js
+++ Dev/src/components/home_page/PopularSection.js
@@ -8,11 +8,13 @@ function PopularSection() {
         <div className='PopularSection'>
             <h1>Titulos de Sucesso no nosso catalogo</h1>
             <Carrossel />
+
             <h1>Perguntas</h1>
-            <h1>Perguntas</h1>
-            <Pergunta pergunta="O que é um título popular?" resposta="Um título popular é um filme ou série que está entre os mais assistidos do nosso catálogo." />
-            <Pergunta pergunta="Como eu posso adicionar um título aos favoritos?" resposta="Você pode clicar no ícone de coração ao lado do título para adicioná-lo à sua lista de favoritos." />
-            <Pergunta pergunta="Posso baixar filmes para assistir offline?" resposta="Sim, você pode baixar títulos selecionados para assistir offline no nosso aplicativo." />
+            <div className='container'>
+                <Pergunta pergunta="?" resposta="R" />
+                <Pergunta pergunta="?" resposta="R" />
+                <Pergunta pergunta="?" resposta="R" />
+            </div>
         </div>
 
     );
