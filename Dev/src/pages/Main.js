389be74268fcf9843843a6ba8ab9f8677fcac6a9
ItalloOtavio <otavio.itallo@gmail.com>
2024-09-29 21:13:23 -0300

    Criação do PopUp e correção Main

--- Dev/src/pages/Main.js
+++ Dev/src/pages/Main.js
@@ -1,20 +1,31 @@
 function Main(){
-  const url = 'https://via.placeholder.com/150'
-    return (
-      <main className="conteiner">
-       <img class="img" src={url} alt="..."></img>
-       <img class="img" src={url} alt="..."></img>
-       <img class="img" src={url} alt="..."></img>
-       <img class="img" src={url} alt="..."></img>
-       <img class="img" src={url} alt="..."></img>
-       <img class="img" src={url} alt="..."></img>
-       <img class="img" src={url} alt="..."></img>
-       <img class="img" src={url} alt="..."></img>
-       <img class="img" src={url} alt="..."></img>
-       <img class="img" src={url} alt="..."></img>
-       <img class="img" src={url} alt="..."></img>
-       <img class="img" src={url} alt="..."></img >
-      </main>
+  const urls = [
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150'
+];
+
+  return (
+    <div>
+            {urls.map((url, index) => (
+                <img
+                    key={index}
+                    src={url}
+                    alt={`Imagem ${index}`}
+                    onClick={() => onImageClick(url)}
+                    style={{ cursor: 'pointer', margin: '10px' }}
+                />
+            ))}
+        </div>
     );  
 }
 export default Main;
