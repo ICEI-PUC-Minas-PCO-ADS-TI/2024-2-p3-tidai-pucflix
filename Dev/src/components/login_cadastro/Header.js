e2e960c1df6705fd7c68047b67b379535531bf57
Caio Ferreira Araujo <caiof13araujo@gmail.com>
2024-10-04 19:52:55 -0300

    .

---
+++ Dev/src/components/login_cadastro/Header.js
@@ -0,0 +1,17 @@
+import Logo from "../../assets/img/PucFlix_Logo.png";
+import { Link } from "react-router-dom";
+import '../../assets/css/login_cadastro/Header.css';
+
+function Header() {
+    return (
+        <header >
+            <Link to="/">
+                <a href="#" className="flex items-center mb-4 font-medium text-gray-900 title-font lg:mb-0">
+                    <img src={Logo} alt="logo" style={{width:220 }} />
+                </a>
+            </Link>
+        </header>
+    );
+}
+
+export default Header;
--- Dev/src/pages/Pucflix.js
+++ Dev/src/pages/Pucflix.js
@@ -1,8 +1,8 @@
 import Principal from "./Principal.js";
 //import Favoritos from "./Favoritos.js";
 import HomePage from "./Home_page.js";
-//import Login from "./Login.js";
-//import Cadastro from "./Cadastro.js";
+import Login from "./Login.js";
+import Cadastro from "./Cadastro.js";
 //import Generos from "./Generos.js";
 //import PagEscolhaPerfil from "./Pag_escolha_perfil.js"
 //import PagGerenciamentoPerfis from "./Pag_gerenciamento_perfis.js"
@@ -17,15 +17,17 @@ function Pucflix() {
         <div className="min-h-screen flex flex-col">
             <BrowserRouter basename="/">
                 <Routes>
-                <Route path="/" element={<AuthLayout />}>
+                    <Route path="/" element={<AuthLayout />}>
                         <Route index element={<HomePage />} />
                     </Route>
+                    <Route path="pucflix/login" element={<Login />} />
+                    <Route path="pucflix/cadastro" element={<Cadastro />} />
 
-                <Route path="pucflix/" element={<PainelLayout />} >
-                    <Route path="principal" element={<Principal />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+                    <Route path="pucflix/" element={<PainelLayout />} >
+                        <Route path="principal" element={<Principal />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </div >
     )
 }
