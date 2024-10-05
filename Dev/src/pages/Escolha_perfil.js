789036d1d412aca6db03f4fe0bee7682bfd1bae8
Caio Ferreira Araujo <caiof13araujo@gmail.com>
2024-10-05 13:30:05 -0300

    links

--- Dev/src/pages/Escolha_perfil.js
+++ Dev/src/pages/Escolha_perfil.js
@@ -1,53 +1,59 @@
 import Cards from "../components/pagina_escolha_perfil/Cards";
 import Header from '../components/template_alternativo/Header/Header.js';
 import Footer from '../components/template_padrao/Footer/Footer.js';
-import styles  from "../assets/css/pagina_escolha_perfil/Pag_escolha_perfil.module.css";
+import styles from "../assets/css/pagina_escolha_perfil/Pag_escolha_perfil.module.css";
+import { Link } from "react-router-dom";
 
-function Pag_escolha_perfil(){
+function Pag_escolha_perfil() {
     const cards = [{
-        nome:'Usuario1',
-        foto:'https://via.placeholder.com/250'
+        nome: 'Usuario1',
+        foto: 'https://via.placeholder.com/250'
     },
     {
-        nome:'Usuario2',
-        foto:'https://via.placeholder.com/250'
+        nome: 'Usuario2',
+        foto: 'https://via.placeholder.com/250'
     },
     {
-        nome:'Usuario3',
-        foto:'https://via.placeholder.com/250'
+        nome: 'Usuario3',
+        foto: 'https://via.placeholder.com/250'
     },
     {
-        nome:'Usuario4',
-        foto:'https://via.placeholder.com/250'
+        nome: 'Usuario4',
+        foto: 'https://via.placeholder.com/250'
     }]
 
-    return(
-        
-        <div>
-            <Header/>
-            < div className={styles.conteudo}>
-                
-                <h1 className={styles.titulo}>Escolha o Perfil</h1>
+    return (
 
-                <div className={styles.cards}>
-                    {cards.map(card => (
-                        <Cards  
-                            nome={card.nome} 
-                            foto={card.foto}
-                        />
-                    ))}
-                </div>
-                
-                    <a href="#"><button className={styles.botao}>Gerenciar Perfis</button></a>
-                  
+        <div>
+            <Header />
+            < div className={styles.conteudo}>
+
+                <h1 className={styles.titulo}>Escolha o Perfil</h1>
+                <Link to="/pucflix/principal">
+                    <div className={styles.cards}>
+                        {cards.map(card => (
+                            <Cards
+                                nome={card.nome}
+                                foto={card.foto}
+                            />
+                        ))}
+                    </div>
+                </Link>
+
+
+
+                <Link to="/pucflix/perfil/edit">
+                    <button className={styles.botao}>Gerenciar Perfis</button>
+                </Link>
+
             </div>
             <Footer />
 
 
-            
+
         </div>
-        
-        
+
+
     )
 }
 
