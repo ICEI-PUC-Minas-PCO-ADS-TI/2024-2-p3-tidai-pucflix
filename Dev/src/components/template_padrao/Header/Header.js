88e887d8b25c223aa85bd7f663327e7396b532ec
Caio Ferreira Araujo <caiof13araujo@gmail.com>
2024-12-08 22:00:42 -0300

    Finalizaçao barra de pesquisa

--- Dev/src/components/template_padrao/Header/Header.js
+++ Dev/src/components/template_padrao/Header/Header.js
@@ -7,13 +7,46 @@ import { IoSearchSharp } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import { getSearchMovies } from "../../../services/TMDB/TMDBFunctions";
+import { getVideoByMovie } from "../../../services/TMDB/TMDBFunctions";
+import { addMovieToWatched, addMovieToFavorites } from "../../../services/firebase/databaseFunctions";
+import Notification from "../../pagina_principal/Notificacao";
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [searchQuery, setSearchQuery] = useState(''); // Texto da pesquisa
-  const [movies, setMovies] = useState([]); // Resultados da pesquisa
-  const [showDropdown, setShowDropdown] = useState(false); // Para mostrar a lista suspensa
-  const [debounceTimeout, setDebounceTimeout] = useState(null); // Variável para armazenar o setTimeout
+  const [searchQuery, setSearchQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [debounceTimeout, setDebounceTimeout] = useState(null);
+  const [notification, setNotification] = useState(null);
+  const [showModal, setShowModal] = useState(false)
+  const [showVideo, setShowVideo] = useState(false)
+  const [movieId, setMovieId] = useState()
+  const [modalMovieName, setModalmovieName] = useState("")
+  const [modalMovieIMG, setModalmovieIMG] = useState("")
+  const [modalMovieDesc, setModalmovieDesc] = useState("")
+  const [modalMovieBio, setmodalMovieBio] = useState("")
+  const [modalVideo, setmodalVideo] = useState("")
+  const [video, setVideoUrl] = useState([]);
+
+  const handleModal = (movieImg, movieName, movieDesc, movieBio, movieVideo, movieId) => {
+    setShowModal(true)
+    setModalmovieDesc(movieDesc)
+    setModalmovieIMG(movieImg)
+    setModalmovieName(movieName)
+    setmodalMovieBio(movieBio)
+    setmodalVideo(movieVideo)
+    setMovieId(movieId)
+    console.log(showModal)
+  }
+
+  const handleShowNotification = () => {
+    setNotification('Filme Adicionado aos Favoritos com Sucesso!');
+
+    setTimeout(() => {
+      setNotification(null);
+    }, 2500);
+  };
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -33,46 +66,28 @@ function Header() {
     };
   }, []);
 
-  const fetchMovies = async (query) => {
-    const allMovies = [
-      { id: 1, title: "Inception" },
-      { id: 2, title: "Interstellar" },
-      { id: 3, title: "The Dark Knight" },
-      { id: 4, title: "Dunkirk" },
-      { id: 5, title: "Memento" },
-      { id: 6, title: "The Prestige" },
-    ];
-
-    return allMovies.filter((movie) =>
-      movie.title.toLowerCase().includes(query.toLowerCase())
-    );
-  };
 
   useEffect(() => {
-    // Atualiza os filmes somente após 2 segundos sem alteração no texto de pesquisa
     const updateMovies = async () => {
       if (searchQuery.trim()) {
-        const filteredMovies = await fetchMovies(searchQuery);
+        const filteredMovies = await getSearchMovies(searchQuery);
+        console.log(filteredMovies)
         setMovies(filteredMovies);
-        setShowDropdown(true);  // Exibe o dropdown
+        setShowDropdown(true);
       } else {
-        setMovies([]);  // Limpa os filmes quando a pesquisa estiver vazia
-        setShowDropdown(false);  // Esconde o dropdown quando não houver texto
+        setMovies([]);
+        setShowDropdown(false);
       }
     };
 
     if (debounceTimeout) {
-      clearTimeout(debounceTimeout); // Limpa o timeout anterior, se existir
+      clearTimeout(debounceTimeout);
     }
 
-    const timeout = setTimeout(updateMovies, 1000); // Aguarda 2 segundos após a última alteração
-
-    setDebounceTimeout(timeout); // Armazena o timeout
-
-    // Limpa o timeout ao desmontar o componente ou ao alterar o estado
+    const timeout = setTimeout(updateMovies, 100);
+    setDebounceTimeout(timeout);
     return () => clearTimeout(timeout);
-
-  }, [searchQuery]);  // Reexecuta a cada mudança no texto de pesquisa
+  }, [searchQuery]);
 
   return (
     <header className="w-full text-white bg-defaultPurple shadow-sm body-font border-b">
@@ -104,7 +119,7 @@ function Header() {
               placeholder="Pesquisar"
               aria-label="Barra de Pesquisa"
               value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)} // Atualiza o estado a cada alteração
+              onChange={(e) => setSearchQuery(e.target.value)}
             />
             <span
               className="inline-flex items-center px-3 text-black bg-white rounded-r-md cursor-pointer"
@@ -117,13 +132,27 @@ function Header() {
           {showDropdown && movies.length > 0 && (
             <ul
               className="absolute mt-1 w-full max-h-60 overflow-y-auto bg-black border border-gray-300 rounded-lg shadow-lg z-20"
-              style={{ top: '65px', width: '260px' }} // Ajuste para a posição e largura
+              style={{ top: '65px', width: '260px' }}
             >
               {movies.map((movie) => (
                 <li
                   key={movie.id}
                   className="px-4 py-3 font-medium border border-gray-300 text-gray-800 hover:bg-indigo-600 hover:text-white cursor-pointer transition-all duration-300 mb-2"
-                  onClick={() => alert(`Você clicou em: ${movie.title}`)} // Exemplo de ação
+                  onClick={() => {
+                    const fetchVideo = async () => {
+                      const video = await getVideoByMovie(movie.id);
+                      handleModal(
+                        `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
+                        movie.title,
+                        movie.overview,
+                        movie.release_date.substring(0, 4) + " | Nota - " + movie.vote_average,
+                        video,  
+                        movie.id
+                      );
+                    };
+                    fetchVideo();
+                  }}
+
                 >
                   {movie.title}
                 </li>
@@ -158,7 +187,6 @@ function Header() {
                   placeholder="Pesquisar"
                   aria-label="Barra de Pesquisa"
                   value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)} // Atualiza o estado a cada alteração
                 />
                 <span
                   className="inline-flex items-center justify-center px-2 text-black border-0 bg-white rounded-r-md h-auto cursor-pointer"
@@ -175,8 +203,93 @@ function Header() {
               </div>
             </nav>
           </div>
+
+
         </div>
       )}
+
+
+      {showModal ? (
+        <>
+          <div
+            className="min-w-64 justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative my-6 mx-auto max-w-3xl">
+              {/*Conteudo*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-defaultBg outline-none focus:outline-none">
+                {/*Header*/}
+                <div className="flex items-start justify-between p-5 rounded-t">
+                  <h3 className="text-3xl font-semibold text-white">
+                    {modalMovieName}
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-white float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => {
+                      setShowModal(false);
+                      setShowVideo(false);
+                    }}
+                  >
+                    <span className="bg-transparent text-white h-6 w-6 text-2xl block outline-none focus:outline-none"
+                    >
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*Body*/}
+                <div className="relative py-1 p-6 flex-auto">
+                  <div className="my-4 text-black text-lg leading-relaxed">
+                    <div className='flex flex-col justify-center md:items-start gap-4 flex-wrap'>
+
+                      {showVideo ? <iframe width="560" height="315" src={modalVideo} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> : <img src={modalMovieIMG} alt={modalMovieIMG} />}
+
+
+                      <div className='flex flex-col gap-5 sm:items-start'>
+                        <div className='flex flex-col items-start text-white text-sm sm:text-base'>
+                          {modalMovieDesc}
+                        </div>
+                        <div className="text-white text-sm sm:text-base">
+                          {modalMovieBio}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/*Footer*/}
+                <div className="flex flex-col items-start justify-start sm:justify-between p-6 rounded-b flex-wrap gap-2">
+                  <button
+                    className="bg-defaultPurple text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => {
+                      addMovieToWatched(movieId)
+                      setShowVideo(true)
+                    }
+                    }
+                  >
+                    Assistir
+                  </button>
+                  <button
+                    className="bg-defaultPurple text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => {
+                      addMovieToFavorites(movieId)
+                      handleShowNotification()
+                    }}
+                  >
+                    Adicionar aos favoritos
+                  </button>
+                  {notification && (
+                    <Notification
+                      message={notification}
+                      onClose={() => setNotification(null)}
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </header>
   );
 }
